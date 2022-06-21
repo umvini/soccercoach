b96691d349d531fa223fada0e0f9b41487ad49f3
Vinicius Ribeiro <38585856+umvini@users.noreply.github.com>
2022-06-20 22:55:16 -0300

    First commit App

---
+++ app/src/main/java/br/com/umvini/soccercoach/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.umvini.soccercoach
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
